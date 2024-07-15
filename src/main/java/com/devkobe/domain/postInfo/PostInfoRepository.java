50bd415c1f3020ba0fa8c5a3807f7b06df10911b
Kobe <dev.skyachieve91@gmail.com>
2024-07-16 08:02:45 +0900

    :sparkles: Feat: 'PostInfoRepository, UserInfoRepository 생성 및 구현.'

---
+++ src/main/java/com/devkobe/domain/postInfo/PostInfoRepository.java
@@ -0,0 +1,5 @@
+package com.devkobe.domain.postInfo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostInfoRepository extends JpaRepository<PostInfo, Long> { }
