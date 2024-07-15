bce2e42db304cd7756da007f9eec95c652e02b0e
Kobe <dev.skyachieve91@gmail.com>
2024-07-16 04:56:38 +0900

    :recycle: Refactor: 'DateInfo 엔티티 수정.'

--- src/main/java/com/devkobe/domain/dateInfo/DateInfo.java
+++ src/main/java/com/devkobe/domain/dateInfo/DateInfo.java
@@ -8,7 +8,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import java.util.Date;
+import jakarta.persistence.OneToOne;
+import java.sql.Timestamp;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,19 +23,27 @@ public class DateInfo {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long dateInfoId;
 
-	@Column(columnDefinition = "DATE", nullable = false)
-	private Date releaseDate;
+	@Column(nullable = false)
+	private Timestamp releaseDate;
 
-	@Column(columnDefinition = "DATE", nullable = false)
-	private Date modificationDate;
+	@Column(nullable = false)
+	private Timestamp modificationDate;
 
-	@ManyToOne
-	@JoinColumn(name = "postId", nullable = false)
+	@OneToOne
+	@JoinColumn(name = "postsId", nullable = false)
 	private Posts posts;
 
 	@Builder
-	public DateInfo(Date releaseDate, Date modificationDate) {
+	public DateInfo(Timestamp releaseDate, Timestamp modificationDate, Posts posts) {
 		this.releaseDate = releaseDate;
 		this.modificationDate = modificationDate;
+		this.posts = posts;
+	}
+
+	public void setPosts(Posts posts) {
+		this.posts = posts;
+		if (posts.getDateInfo() != this) {
+			posts.setDateInfo(this);
+		}
 	}
 }
