40f581e9f6a0b300532c458958b7ddfcf8fc58ba
Kobe <dev.skyachieve91@gmail.com>
2024-07-16 05:00:21 +0900

    :sparkles: Feat: 'DateInfoRepository 생성 및 구현.'

---
+++ src/main/java/com/devkobe/domain/dateInfo/DateInfoRepository.java
@@ -0,0 +1,5 @@
+package com.devkobe.domain.dateInfo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DateInfoRepository extends JpaRepository<DateInfo, Long> { }
