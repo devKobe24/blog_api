8795e6ad21fda65f4994c40fbe37503a0b2ff881
Kobe <dev.skyachieve91@gmail.com>
2024-07-16 05:01:04 +0900

    :sparkles: Feat: 'UserInfoRepository 생성 및 구현.'

---
+++ src/main/java/com/devkobe/domain/userInfo/UserInfoRepository.java
@@ -0,0 +1,5 @@
+package com.devkobe.domain.userInfo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserInfoRepository extends JpaRepository<UserInfo, Long> { }
