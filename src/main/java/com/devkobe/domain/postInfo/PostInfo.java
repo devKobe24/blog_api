8288b1949cc811da0d4e68db205e8d222bb666bc
Kobe <dev.skyachieve91@gmail.com>
2024-07-16 04:57:30 +0900

    :recycle: Refactor: 'PostsInfo 엔티티 수정.'

--- src/main/java/com/devkobe/domain/postInfo/PostInfo.java
+++ src/main/java/com/devkobe/domain/postInfo/PostInfo.java
@@ -8,6 +8,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,23 +22,31 @@ public class PostInfo {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long postInfoId;
 
-	@Column(length = 500, nullable = false)
+	@Column(nullable = false)
 	private String title;
 
-	@Column(columnDefinition = "TEXT", nullable = false)
+	@Column(nullable = false)
 	private String content;
 
-	@Column(columnDefinition = "INT", nullable = false)
+	@Column(nullable = false)
 	private Integer postNumber;
 
-	@ManyToOne
+	@OneToOne
 	@JoinColumn(name = "postsId", nullable = false)
 	private Posts posts;
 
 	@Builder
-	public PostInfo(String title, String content, Integer postNumber) {
+	public PostInfo(String title, String content, Integer postNumber, Posts posts) {
 		this.title = title;
 		this.content = content;
 		this.postNumber = postNumber;
+		this.posts = posts;
+	}
+
+	public void setPosts(Posts posts) {
+		this.posts = posts;
+		if (posts.getPostInfo() != this) {
+			posts.setPostInfo(this);
+		}
 	}
 }
