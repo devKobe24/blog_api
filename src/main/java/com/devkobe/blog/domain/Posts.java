0ff2e888a9e52eeae3627e27af9b4f6bbe168144
Kobe <dev.skyachieve91@gmail.com>
2024-07-16 11:53:40 +0900

    :sparkles: Feat: 'Posts 엔티티 클래스 생성.'

---
+++ src/main/java/com/devkobe/blog/domain/Posts.java
@@ -0,0 +1,36 @@
+package com.devkobe.blog.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Posts {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String title;
+
+	@Column(nullable = false)
+	private String content;
+
+	@Column(nullable = false)
+	private String author;
+
+	@Builder
+	public Posts(String title, String content, String author) {
+		this.title = title;
+		this.content = content;
+		this.author = author;
+	}
+}
