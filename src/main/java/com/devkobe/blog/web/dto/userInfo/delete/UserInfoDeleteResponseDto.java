e6428429093727a801e59c28df42c21e419182c9
Kobe <dev.skyachieve91@gmail.com>
2024-07-18 03:53:19 +0900

    :recycle: Refactor: '@Builder 어노테이션 추가.'

--- src/main/java/com/devkobe/blog/web/dto/userInfo/delete/UserInfoDeleteResponseDto.java
+++ src/main/java/com/devkobe/blog/web/dto/userInfo/delete/UserInfoDeleteResponseDto.java
@@ -1,5 +1,6 @@
 package com.devkobe.blog.web.dto.userInfo.delete;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +10,7 @@ public class UserInfoDeleteResponseDto {
 	private Long id;
 	private String status;
 
+	@Builder
 	public UserInfoDeleteResponseDto(Long id, String status) {
 		this.id = id;
 		this.status = status;
