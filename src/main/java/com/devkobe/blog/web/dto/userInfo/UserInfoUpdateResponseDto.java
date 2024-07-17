b7ab2a1e825b4edaf5814cadb97a18cf63edb4b1
Kobe <dev.skyachieve91@gmail.com>
2024-07-17 15:32:58 +0900

    :sparkles: Feat: 'UserInfoUpdateResponseDto 클래스 생성 및 구현.'

---
+++ src/main/java/com/devkobe/blog/web/dto/userInfo/UserInfoUpdateResponseDto.java
@@ -0,0 +1,21 @@
+package com.devkobe.blog.web.dto.userInfo;
+
+import com.devkobe.blog.domain.UserInfo;
+import lombok.Getter;
+
+@Getter
+public class UserInfoUpdateResponseDto {
+	private Long id;
+	private String name;
+	private String email;
+	private String profileIamge;
+	private String nickName;
+
+	public UserInfoUpdateResponseDto(UserInfo entity) {
+		this.id = entity.getId();
+		this.name = entity.getName();
+		this.email = entity.getEmail();
+		this.profileIamge = entity.getProfileImage();
+		this.nickName = entity.getNickName();
+	}
+}
