6b6629ffa24302ce183e39d203fdaa09d51afe7e
Kobe <dev.skyachieve91@gmail.com>
2024-07-18 03:43:50 +0900

    :sparkles: Feat: 'UserInfoReadResponseDto 생성 및 구현.'

---
+++ src/main/java/com/devkobe/blog/web/dto/userInfo/read/UserInfoReadResponseDto.java
@@ -0,0 +1,25 @@
+package com.devkobe.blog.web.dto.userInfo.read;
+
+import com.devkobe.blog.domain.UserInfo;
+import com.devkobe.blog.web.dto.userInfo.UserInfoResponseDto;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class UserInfoReadResponseDto {
+
+	private Long id;
+	private String name;
+	private String email;
+	private String profileImage;
+	private String nickName;
+
+	@Builder
+	public UserInfoReadResponseDto(UserInfo entity) {
+		this.id = entity.getId();
+		this.name = entity.getName();
+		this.email = entity.getEmail();
+		this.profileImage = entity.getProfileImage();
+		this.nickName = entity.getNickName();
+	}
+}
