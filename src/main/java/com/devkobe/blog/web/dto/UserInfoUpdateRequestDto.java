942c83d187156d59b1d3997b8dd86157afa71980
Kobe <dev.skyachieve91@gmail.com>
2024-07-17 12:48:44 +0900

    :sparkles: Feat: 'UserInfoUpdateRequestDto 생성 및 구현.'

---
+++ src/main/java/com/devkobe/blog/web/dto/UserInfoUpdateRequestDto.java
@@ -0,0 +1,23 @@
+package com.devkobe.blog.web.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class UserInfoUpdateRequestDto {
+	private String name;
+	private String email;
+	private String profileImage;
+	private String nickName;
+
+	@Builder
+	public UserInfoUpdateRequestDto(String name, String email, String profileImage, String nickName) {
+		this.name = name;
+		this.email = email;
+		this.profileImage = profileImage;
+		this.nickName = nickName;
+	}
+
+}
