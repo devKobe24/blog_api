d664e861fb2e11c958dcdad23453ad01492339f1
Kobe <dev.skyachieve91@gmail.com>
2024-07-18 10:38:49 +0900

    :recycle: Refactor: 'JpaSpecificationExecutor 확장.'
    JpaSepecificationExecutor로 확장하여 동적 검색 기능을 지원하게 하였습니다.

--- src/main/java/com/devkobe/blog/repository/PostsRepository.java
+++ src/main/java/com/devkobe/blog/repository/PostsRepository.java
@@ -2,5 +2,8 @@ package com.devkobe.blog.repository;
 
 import com.devkobe.blog.domain.Posts;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface PostsRepository extends JpaRepository<Posts, Long> { }
+public interface PostsRepository extends JpaRepository<Posts, Long>, JpaSpecificationExecutor<Posts> {
+	// 필요에 따라 추가적인 커스텀 쿼리 메서드를 정의.
+}
