d3294d1574a30c8a49621c53d85e6bf5b23b95cb
Kobe <dev.skyachieve91@gmail.com>
2024-07-18 10:42:36 +0900

    :pencil2: Chore:

--- src/main/java/com/devkobe/blog/web/dto/posts/read/PostsReadRequestDto.java
+++ src/main/java/com/devkobe/blog/web/dto/posts/read/PostsReadRequestDto.java
@@ -1,6 +1,5 @@
 package com.devkobe.blog.web.dto.posts.read;
 
-import com.devkobe.blog.domain.Posts;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,10 +7,22 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class PostsReadRequestDto {
-	private Long id;
+
+	private String title;
+	private String content;
+	private String nickName;
+	private Integer postNumber;
+	private String additionalInfo;
+	private Long userInfoId;
 
 	@Builder
-	public PostsReadRequestDto(Posts entity) {
-		this.id = entity.getId();
+	public PostsReadRequestDto(String title, String content, String nickName, Integer postNumber, String additionalInfo, Long userInfoId) {
+
+		this.title = title;
+		this.content = content;
+		this.nickName = nickName;
+		this.postNumber = postNumber;
+		this.additionalInfo = additionalInfo;
+		this.userInfoId = userInfoId;
 	}
 }
