27493c822f073fae6de804d9579c288633abb79c
Kobe <dev.skyachieve91@gmail.com>
2024-07-19 10:51:01 +0900

    :sparkles: Feat:

---
+++ src/main/java/com/devkobe/blog/repository/DateInfoRepository.java
@@ -0,0 +1,6 @@
+package com.devkobe.blog.repository;
+
+import com.devkobe.blog.domain.DateInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DateInfoRepository extends JpaRepository<DateInfo, Long> { }
