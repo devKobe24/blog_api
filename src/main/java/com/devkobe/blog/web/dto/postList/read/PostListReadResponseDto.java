c0393826806aa43fb2b85382e6ba578ccc6e5423
Kobe <dev.skyachieve91@gmail.com>
2024-07-25 11:11:43 +0900

    :recycle: Refactor: 'postId를 UUID로 교체.'

--- src/main/java/com/devkobe/blog/web/dto/postList/read/PostListReadResponseDto.java
+++ src/main/java/com/devkobe/blog/web/dto/postList/read/PostListReadResponseDto.java
@@ -3,6 +3,7 @@ package com.devkobe.blog.web.dto.postList.read;
 import com.devkobe.blog.domain.post.Post;
 import com.devkobe.blog.domain.postList.PostList;
 import com.devkobe.blog.web.dto.post.read.PostReadResponseDto;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.UUID;
 import java.util.stream.Collectors;
 import lombok.Getter;
@@ -11,13 +12,13 @@ import java.util.List;
 
 @Getter
 public class PostListReadResponseDto {
-//    private UUID uuid;
-    private Long postId;
+
+    @JsonProperty("uuid")
+    private UUID uuid;
     private List<PostReadResponseDto> posts;
 
     public PostListReadResponseDto(PostList entity) {
-//        this.uuid = entity.getUuid();
-        this.postId = entity.getPostId();
+        this.uuid = entity.getUuid();
         this.posts = entity
             .getPosts()
             .stream()
